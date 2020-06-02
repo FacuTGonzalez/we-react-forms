c63495fa764af591d5962157605378a156316373
facu velasco <velascofacundo@gmail.com>
2020-06-02 15:02:03 -0300

    fix: get undefined/image bug

--- browser/react/components/SingleAlbum.jsx
+++ browser/react/components/SingleAlbum.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import Songs from './Songs';
+import React from "react";
+import Songs from "./Songs";
 
 export default class SingleAlbum extends React.Component {
-
   componentDidMount() {
-    this.props.selectAlbum(this.props.albumId)
+    this.props.selectAlbum(this.props.albumId);
   }
 
   render() {
@@ -13,13 +12,12 @@ export default class SingleAlbum extends React.Component {
       <div className="album">
         <div>
           <h3>{album.name}</h3>
-          <img src={`/api/albums/${album.id}/image`} className="img-thumbnail" />
+          <img
+            src={album.id ? `/api/albums/${album.id}/image` : ""}
+            className="img-thumbnail"
+          />
         </div>
-        <Songs 
-          songs={album.songs}
-          start={start}
-          selectedSong={selectedSong}
-        />
+        <Songs songs={album.songs} start={start} selectedSong={selectedSong} />
       </div>
     );
   }
